3297acbdcfd35d37535fcf7bdee8492c3e53e475
slacksky <slacksky@gmail.com>
2021-07-04 21:12:02 -0500

    added c3po, example 1 out of 5 resolved

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-class Main {
+public class Main {
+  static void method_c3po() {
+    System.out.println("Ejercicio C3PO \n");
+    System.out.println("   /~\\ ");
+    System.out.println("  |oo ). ");
+    System.out.println("  _\\=/_");
+    System.out.println(" /  _  \\ ");
+    System.out.println("//|/.\\|\\\\ ");
+    System.out.println("\\\\ \\_/  || ");
+    System.out.println(" \\|\\ /| || ");
+    System.out.println(" # _ _/  #");
+    System.out.println("  | | |");
+    System.out.println("  | | |");
+    System.out.println("  []|[]");
+    System.out.println("  | | |");
+    System.out.println("_/_]_[_\\___");
+
+    System.out.println("/----cierre dibujar C3PO-----/ ");
+
+  }
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    method_c3po();//aqui invocamos el methodo para el poster de  C3PO
   }
 }
